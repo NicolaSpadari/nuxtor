517fc8ffdcce0c7d6ff6068c9f01816986b629a0
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-06-16 12:48:33 +0200

    Add more apis

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,9 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-
 pub fn run() {
     tauri::Builder::default()
 		.plugin(tauri_plugin_shell::init())
+		.plugin(tauri_plugin_notification::init())
+		.plugin(tauri_plugin_os::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,16 +1,38 @@
 <template>
 	<div text-center space-y-5>
-		Hello from Nuxtor - Tauri v{{ version }}
+		<p>Hello from Nuxtor - Tauri v{{ version }}</p>
+		<p>Running on {{ currentOS }}</p>
 
 		<p>Shell command result: {{ result }}</p>
+
+		<button @click="runNotification()">
+			send notification
+		</button>
 	</div>
 </template>
 
 <script lang="ts" setup>
+	const currentOS = await platform();
 	const version = await getTauriVersion();
 
 	const result = await Command.create("exec-sh", [
 		"-c",
 		"echo 'Hello World!'"
 	]).execute();
+
+	const runNotification = async () => {
+		let permissionGranted = await isPermissionGranted();
+
+		if (!permissionGranted) {
+			const permission = await requestPermission();
+			permissionGranted = permission === "granted";
+		}
+
+		if (permissionGranted) {
+			sendNotification({
+				title: "Hello Tauri",
+				body: "Notification from tauri"
+			});
+		}
+	};
 </script>
