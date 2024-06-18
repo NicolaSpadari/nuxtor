a2c536c0ab61262e754cc433dae6a9b007123170
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-06-18 22:41:47 +0200

    Update tauri website url

--- src/composables/constants.ts
+++ src/composables/constants.ts
@@ -2,7 +2,7 @@ export const useConstants = () => {
 	const name = "Nuxtor";
 	const author = "Nicola Spadari";
 	const repo = "https://github.com/NicolaSpadari";
-	const tauriSite = "https://tauri.app";
+	const tauriSite = "https://v2.tauri.app";
 	const nuxtSite = "https://nuxt.com";
 	const unoSite = "https://unocss.dev";
 
