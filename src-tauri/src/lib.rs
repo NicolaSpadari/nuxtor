85f9de7a0f0b4b0a4761acee46f15dff0069ef95
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-10-08 13:55:56 +0200

    Add tray icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,34 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
+
+#[allow(unused)]
+
+use tauri::{
+	menu::{Menu, MenuItem},
+	tray::TrayIconBuilder
+};
+
 pub fn run() {
     tauri::Builder::default()
+		.setup(|app| {
+			let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+			let menu = Menu::with_items(app, &[&quit_i])?;
+
+			let tray = TrayIconBuilder::new()
+				.menu(&menu)
+				.menu_on_left_click(true)
+				.icon(app.default_window_icon().unwrap().clone())
+				.on_menu_event(|app, event| match event.id.as_ref() {
+					"quit" => {
+						app.exit(0);
+					}
+					other => {
+						println!("menu item {} not handled", other);
+					}
+				})
+				.build(app)?;
+
+			Ok(())
+		})
 		.plugin(tauri_plugin_shell::init())
 		.plugin(tauri_plugin_notification::init())
 		.plugin(tauri_plugin_os::init())
