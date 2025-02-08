079078d75adba483c9218dd153519dbaae00253f
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2025-02-08 14:34:35 +0100

    Fix homepage restyle

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,25 +3,49 @@
 		<div class="grid size-full place-content-center gap-y-8">
 			<SvgoLogo :filled="true" :font-controlled="false" class="mx-auto size-40" />
 
-			<div class="flex flex-col items-center gap-y-5 md:gap-y-7">
-				<h1 class="animate-pulse text-3xl sm:text-4xl text-pretty font-bold text-(--ui-text-highlighted) font-heading">
+			<div class="flex flex-col items-center gap-y-3">
+				<h1 class="animate-pulse text-3xl sm:text-4xl text-pretty font-bold text-(--ui-text-highlighted) font-heading md:mb-5">
 					{{ app.name.toUpperCase() }}
 				</h1>
-				<p class="leading-7 text-pretty">
+				<p class="leading-7 text-pretty text-center">
 					Powered by
-					<ULink :to="app.nuxtSite" target="_blank" :external="true">
-						Nuxt 3
-					</ULink>
-					-
-					<ULink :to="app.tauriSite" target="_blank" :external="true">
-						Tauri 2
-					</ULink>
-					-
-					<ULink :to="app.nuxtUiSite" target="_blank" :external="true">
-						NuxtUI 3
-					</ULink>
 				</p>
-				<UButton :to="app.repo">
+
+				<div class="flex flex-wrap justify-center gap-1 md:gap-3">
+					<UButton
+						variant="ghost"
+						size="xl"
+						:to="app.nuxtSite"
+						target="_blank"
+						:external="true"
+					>
+						Nuxt 3
+					</UButton>
+					<UButton
+						variant="ghost"
+						size="xl"
+						:to="app.nuxtSite"
+						target="_blank"
+						:external="true"
+					>
+						Nuxt 3
+					</UButton>
+					<UButton
+						variant="ghost"
+						size="xl"
+						:to="app.nuxtSite"
+						target="_blank"
+						:external="true"
+					>
+						Nuxt 3
+					</UButton>
+				</div>
+			</div>
+
+			<div class="flex justify-center">
+				<UButton
+					:to="app.repo"
+				>
 					Star on GitHub
 				</UButton>
 			</div>
